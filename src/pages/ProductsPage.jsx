521b54e54137bcaaaf8f1647c1941f5049eba94d
FedericoCanale <icocanale@gmail.com>
2025-11-14 13:11:31 +0100

    Add axios to get products info on products page (no style, only names and prices)

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function ProductsPage() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products")
+            .then(res => {
+                setProducts(res.data);
+            });
+    }, []);
+
     return (
         <div>
             <h1>Prodotti</h1>
 
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.title} - {product.price}€
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
