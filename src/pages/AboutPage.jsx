27f7ecc4cb6a4e263849cd64b81678e108c47fa8
FedericoCanale <icocanale@gmail.com>
2025-11-14 14:16:48 +0100

    Style AboutPage with simple info box

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,21 @@
 export default function AboutPage() {
     return (
-        <div>
-            <h1>Chi siamo</h1>
+        <div className="about-page">
+            <div className="about-box">
+                <h1 className="about-title">Chi Siamo</h1>
 
+                <p className="about-text">
+                    Questo e commerce è stao creato per vendere un po di tutto
+                </p>
+
+                <p className="about-text">
+                    Mi ero stancato di non riuscire a torvare un negozio online che vendesse cose completamente scollegate una dall'altra e così l'ho creato io
+                </p>
+
+                <p className="about-text">
+                    Le prossime aggiunte saranno piatti per la casa e accessori per sanitari. resta colleggato!
+                </p>
+            </div>
         </div>
     );
 }
