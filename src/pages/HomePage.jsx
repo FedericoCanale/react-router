98ed96dfdd60598f09e50d3349e421447b2e34dd
FedericoCanale <icocanale@gmail.com>
2025-11-14 14:01:13 +0100

    Add simple styled welcome section to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 export default function HomePage() {
     return (
-        <div>
-            <h1>Homepage</h1>
-            <p>Benvenuto!</p>
+        <div className="home-page">
+            <div className="home-box">
+                <h1 className="home-title">Benvenuto nel mio piccolo E-Commerce</h1>
+                <p className="home-subtitle">
+                    Qui puoi trovare prodotti di ogni genere, ma veramente di ogni genere! Chi mai venderebbe nello stesso negozio vestiti, zaini e accessori per computer??? Solo io potrei e infatti lo faccio!
+                </p>
+            </div>
         </div>
     );
 }
