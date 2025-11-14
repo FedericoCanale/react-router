a517f6a57155d51dca1cbc07784a9e20d38e53a8
FedericoCanale <icocanale@gmail.com>
2025-11-14 14:56:34 +0100

    Add style to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/chi-siamo">Chi siamo</Link> |{" "}
-            <Link to="/prodotti">Prodotti</Link>
+        <nav className="navbar">
+            <div className="nav-container">
+
+
+                <div className="nav-links">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/chi-siamo">Chi siamo</Link>
+                    <Link className="nav-link" to="/prodotti">Prodotti</Link>
+                </div>
+            </div>
         </nav>
     );
 }
