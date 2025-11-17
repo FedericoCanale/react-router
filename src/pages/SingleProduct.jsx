4ae786f77126545f3d8b5be5aec752b47eeae8fd
FedericoCanale <icocanale@gmail.com>
2025-11-17 12:51:43 +0100

    Add SingleProduct page with axios fetch

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,12 +1,40 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SingleProduct() {
     const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then(res => {
+                setProduct(res.data);
+            });
+    }, [id]);
+
+    if (!product) {
+        return (
+            <div>
+                <h1>Prodotto in caricamento</h1>
+
+            </div>
+        );
+    }
 
     return (
         <div>
-            <h1>Pagina prodotto singolo</h1>
-            <p>ID: {id}</p>
+            <h1>{product.title}</h1>
+
+            <img
+                src={product.image}
+                alt={product.title}
+                width="200"
+            />
+
+            <p><strong>Prezzo:</strong> {product.price} €</p>
+            <p><strong>Categoria:</strong> {product.category}</p>
+            <p><strong>Descrizione:</strong> {product.description}</p>
         </div>
     );
 }
